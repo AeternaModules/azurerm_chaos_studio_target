714f534dc059499319993dc1f36d0db4b8952cb0
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:38 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "chaos_studio_targets" {
   description = "All chaos_studio_target resources"
   value       = azurerm_chaos_studio_target.chaos_studio_targets
 }
+output "chaos_studio_targets_location" {
+  description = "List of location values across all chaos_studio_targets"
+  value       = [for k, v in azurerm_chaos_studio_target.chaos_studio_targets : v.location]
+}
+output "chaos_studio_targets_target_resource_id" {
+  description = "List of target_resource_id values across all chaos_studio_targets"
+  value       = [for k, v in azurerm_chaos_studio_target.chaos_studio_targets : v.target_resource_id]
+}
+output "chaos_studio_targets_target_type" {
+  description = "List of target_type values across all chaos_studio_targets"
+  value       = [for k, v in azurerm_chaos_studio_target.chaos_studio_targets : v.target_type]
+}
 
