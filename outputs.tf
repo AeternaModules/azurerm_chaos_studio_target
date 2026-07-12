9445744e2d57c2d787e814e5cc613926d8688b40
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:23 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "chaos_studio_targets" {
-  description = "All chaos_studio_target resources"
-  value       = azurerm_chaos_studio_target.chaos_studio_targets
-}
 output "chaos_studio_targets_location" {
-  description = "List of location values across all chaos_studio_targets"
-  value       = [for k, v in azurerm_chaos_studio_target.chaos_studio_targets : v.location]
+  description = "Map of location values across all chaos_studio_targets, keyed the same as var.chaos_studio_targets"
+  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.location }
 }
 output "chaos_studio_targets_target_resource_id" {
-  description = "List of target_resource_id values across all chaos_studio_targets"
-  value       = [for k, v in azurerm_chaos_studio_target.chaos_studio_targets : v.target_resource_id]
+  description = "Map of target_resource_id values across all chaos_studio_targets, keyed the same as var.chaos_studio_targets"
+  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.target_resource_id }
 }
 output "chaos_studio_targets_target_type" {
-  description = "List of target_type values across all chaos_studio_targets"
-  value       = [for k, v in azurerm_chaos_studio_target.chaos_studio_targets : v.target_type]
+  description = "Map of target_type values across all chaos_studio_targets, keyed the same as var.chaos_studio_targets"
+  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.target_type }
 }
 
