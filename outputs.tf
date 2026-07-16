55642e72c727c3e36f58dd0682e5e959f41db306
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "chaos_studio_targets_id" {
   description = "Map of id values across all chaos_studio_targets, keyed the same as var.chaos_studio_targets"
-  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.id }
+  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "chaos_studio_targets_location" {
   description = "Map of location values across all chaos_studio_targets, keyed the same as var.chaos_studio_targets"
-  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.location }
+  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "chaos_studio_targets_target_resource_id" {
   description = "Map of target_resource_id values across all chaos_studio_targets, keyed the same as var.chaos_studio_targets"
-  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.target_resource_id }
+  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.target_resource_id if v.target_resource_id != null && length(v.target_resource_id) > 0 }
 }
 output "chaos_studio_targets_target_type" {
   description = "Map of target_type values across all chaos_studio_targets, keyed the same as var.chaos_studio_targets"
-  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.target_type }
+  value       = { for k, v in azurerm_chaos_studio_target.chaos_studio_targets : k => v.target_type if v.target_type != null && length(v.target_type) > 0 }
 }
 
